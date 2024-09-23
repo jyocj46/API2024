d7eed5d7990dd439987cd227b93fb8a6182e2bcd
jyocj46 <89291658+jyocj46@users.noreply.github.com>
2024-09-23 14:25:34 -0600

    Update env.js

--- app/config/env.js
+++ app/config/env.js
@@ -1,10 +1,10 @@
 
 
 const env = {
-  database: 'umg_ejercicio1',
-  username: 'umg_ejercicio_user',
-  password: 'Il0dFPqljh555fVSXnUg1lbWeJeHKlIm',
-  host: 'dpg-cqgr7qlds78s73aqfau0-a',
+  database: 'parcial_2024',
+  username: 'parcial_2024_user',
+  password: '79cg3KUz3hxeGhovdWExEc6S6YMYf13g',
+  host: 'dpg-crorpce8ii6s739prbmg-a',
   dialect: 'postgres',
   pool: {
     max: 5,
